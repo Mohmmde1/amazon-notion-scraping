eb0d2a2e672ae745380045e0a0097e98f7e315bf
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2022-10-08 21:20:03 +0800

    connected the scraper with the api

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from objs.notion import Notion
 from utils.utils import fill_prop
-from objs.Book import Book
+from objs.book import Book
 from objs.amazon import AmazonScraper
-from pprint import pprint
 
 # url = input("Insert the book's url=> ")
 file_name = "./json/files/db_create.json"
+URL = "https://www.amazon.com/Zero-One-Notes-Startups-Future-ebook/dp/B00J6YBOFQ/ref=sr_1_1?crid=FUJBV9FJYCE9&keywords=zero+to+one&qid=1665234827&qu=eyJxc2MiOiIyLjAzIiwicXNhIjoiMS44NCIsInFzcCI6IjIuMDUifQ%3D%3D&s=digital-text&sprefix=zero+to+one%2Cdigital-text%2C596&sr=1-1"
+
+# initiolize the url
+AmazonScraper.initialize(URL)
+book = AmazonScraper.scrape()
 
 # AmazonScraper.scrape(url)
-data_dict = fill_prop(file_name, Book())
+data_dict = fill_prop(file_name, book)
+
+# add a record
 Notion.create(data=data_dict)
--- objs/amazon.py
+++ objs/amazon.py
@@ -1,4 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+from objs.book import Book
+
 class AmazonScraper:
-    @staticmethod
-    def scrape(url):
-        pass
+    url = None
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    page = None
+    soup = None
+    
+    @classmethod
+    def initialize(cls, url):
+        cls.url = url
+        cls.page = requests.get(url, headers=cls.headers)
+        print(cls.page)
+        cls.soup = BeautifulSoup(cls.page.content, "html.parser")
+    
+    @classmethod
+    def scrape(cls):
+        stars_emjos = {1:"⭐️", 2:"⭐️⭐️", 3:"⭐️⭐️⭐️", 4:"⭐️⭐️⭐️⭐️", 5:"⭐️⭐️⭐️⭐️⭐️"}
+        book = Book(score=stars_emjos[int(round(float(cls.get_score()[0])))], 
+                    link=cls.url, author=cls.get_author(),
+                    page_count=cls.get_page_count(), name=cls.get_name(), 
+                    puplisher=cls.get_puplisher())
+        return book
+    
+    @classmethod
+    def get_score(cls):
+        score = cls.soup.find("span", {"class": "a-icon-alt"})
+        return score.getText()
+
+    @classmethod
+    def get_link(cls):
+        return cls.url
+
+    @classmethod
+    def get_author(cls):
+        name = cls.soup.find("a", {"class" : "a-size-base a-link-normal a-text-normal"})
+        return name.getText()
+        
+
+    @classmethod
+    def get_page_count(cls):
+        div = cls.soup.find("div", {"class":"a-section a-spacing-none a-text-center rpi-attribute-value"})
+        page_count = div.findChild("span")
+        return int(page_count.getText().split()[0])
+
+    @classmethod
+    def get_name(cls):
+        title = cls.soup.findAll("span", {"id":"productTitle"})[0]
+        return title.get_text()
+
+
+    @classmethod
+    def get_type(cls):
+        return "book"
+
+    @classmethod
+    def get_puplisher(cls):
+        div = cls.soup.findAll("div", {"class":"a-section a-spacing-none a-text-center rpi-attribute-value"})[2]
+        puplisher = div.findChild("span")
+        return puplisher.getText()
+
+
+    @classmethod
+    def get_release_date(cls):
+        div = cls.soup.findAll("div", {"class":"a-section a-spacing-none a-text-center rpi-attribute-value"})[3]
+        release_date = div.findChild("span")
+        return release_date.getText()
+
+    
+        
+        
